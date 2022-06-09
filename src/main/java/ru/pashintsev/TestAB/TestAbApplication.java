d7e37a5ece4200cf277efb1bcb2d5b83fcc33831
Artem Pashintsev <d1garch@gmail.com>
2022-06-09 14:21:42 +0300

    First commit

---
+++ src/main/java/ru/pashintsev/TestAB/TestAbApplication.java
@@ -0,0 +1,13 @@
+package ru.pashintsev.TestAB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAbApplication.class, args);
+	}
+
+}
