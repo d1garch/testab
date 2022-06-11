9dec2a6b190a8d653efe8eff3c8085983197b896
Artem Pashintsev <d1garch@gmail.com>
2022-06-11 14:04:22 +0300

    Added Values.class

--- src/main/java/ru/pashintsev/TestAB/controller/TestABController.java
+++ src/main/java/ru/pashintsev/TestAB/controller/TestABController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.pashintsev.TestAB.Values;
 import ru.pashintsev.TestAB.model.Exchange;
 import ru.pashintsev.TestAB.service.FeignService;
 
@@ -23,10 +24,12 @@ public class TestABController {
     private ObjectMapper mapper;
     @Autowired
     private Exchange exchange;
+    @Autowired
+    private Values values;
 
     @GetMapping("/old-value")
     public String getOldValue() {
-        String jsonString = feignService.getHistoricalValue();;
+        String jsonString = feignService.getHistoricalValue();
         try{
             exchange = mapper.readValue(jsonString, Exchange.class);
         }
@@ -38,9 +41,33 @@ public class TestABController {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-        return exchange.getRates("RUB").toString();
+        Double oldValue = exchange.getRatesValue("RUB");
+        values.setOldValue(oldValue);
+
+        return oldValue.toString();
     }
 
+    @GetMapping("/latest-value")
+    public String getLatestValue() {
+        String jsonString = feignService.getLatestValue();
+        try{
+            exchange = mapper.readValue(jsonString, Exchange.class);
+        }
+        catch (JSONException e) {
+            return "Something wrong with JSON answer from server";
+        }
+        catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        Double latestValue = exchange.getRatesValue("RUB");
+        values.setNewValue(latestValue);
 
+        if(values.getNewValue()> values.getOldValue()) {
+            return "I'm RICH!!!";
+        }
+        else return "I'm POOR!!!(((";
+    }
 
 }
--- src/main/java/ru/pashintsev/TestAB/model/Exchange.java
+++ src/main/java/ru/pashintsev/TestAB/model/Exchange.java
@@ -1,6 +1,8 @@
 package ru.pashintsev.TestAB.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -12,7 +14,41 @@ public class Exchange {
     private String base;
     private HashMap<String, Double> rates;
 
-    public Double getRates(String key) {
+    public HashMap<String, Double> getRates() {
+        return rates;
+    }
+
+    public Double getRatesValue(String key) {
         return rates.get(key);
     }
+
+    public String getDisclaimer() {
+        return disclaimer;
+    }
+
+    public void setDisclaimer(String disclaimer) {
+        this.disclaimer = disclaimer;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public void setRates(HashMap<String, Double> rates) {
+        this.rates = rates;
+    }
+
+
 }
